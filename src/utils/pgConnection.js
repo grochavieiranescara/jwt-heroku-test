244ba9c45f67f5899cc4ab89192cb6a6df7a82d5
grochavieiranescara <guilherme.rocha@nescara.com>
2022-07-22 16:00:05 -0300

    ssl added

--- src/utils/pgConnection.js
+++ src/utils/pgConnection.js
@@ -1,13 +1,15 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-// const connectionString = process.env.DATABASE_URL;
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const connectionString = process.env.DATABASE_URL;
 
 export async function executeDB(queryCommand) {
   console.log("inicio do pool");
   const pool = new Pool({
     connectionString,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
   const queryResult = await pool.query(queryCommand);
   await pool.end();
