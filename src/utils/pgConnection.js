f15cb30526780ddb38dde7f6b6bd20eb752bd30f
grochavieiranescara <guilherme.rocha@nescara.com>
2022-07-22 15:56:24 -0300

    commit

--- src/utils/pgConnection.js
+++ src/utils/pgConnection.js
@@ -1,7 +1,8 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const connectionString = process.env.DATABASE_URL;
+// const connectionString = process.env.DATABASE_URL;
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 export async function executeDB(queryCommand) {
   console.log("inicio do pool");
